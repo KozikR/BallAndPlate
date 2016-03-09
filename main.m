77ca0a6620a79686fc2f902820558bdc98cb2fe6
Rafał Kozik <KozikR@users.noreply.github.com>
2016-03-09 15:03:41 +0100

    parameters

--- main.m
+++ main.m
@@ -3,13 +3,19 @@ clear all; close all;
 format compact; format long e;
 
 %% Data - initial state, parameters, time, control function
-T = 1;
-x0 = [0, 1];
+T = 1;  % time of simulation
+x0 = [0.1, 0, 0.1, 0]; % position x, velocity x, position y, velocity y 
 
-% control function
+% models parameters
+M = 0.040;      % mass of ball, kg
+R = 0.0015;     % Radius of ball, m
+I = 2/5*M*R^2;  % Moment of inertia of ball
+g = 9.81;       % gravitational acceleration
+
+% control function - angular position of table
 step = 10;  % number of przedzia³y strukturalne -> znaleœæ t³umaczenie
 tau = linspace(0, T, step+1);
-u = sin(tau);
+u = [sin(tau), cos(tau)];
 
 % step in RK4, ~0.01
 dtau = diff(tau);
--- rhs.m
+++ rhs.m
@@ -1,1 +1,6 @@
-function [] = rhs()
+function [dx] = rhs(x, t, u, M, R, I, g)
+dx = [0,0,0,0];
+dx(1) = 
+dx(2) = 
+dx(3) = 
+dx(4) = 
