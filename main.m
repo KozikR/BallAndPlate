c133ee69cb8c4681aad7630f303b4c240671f3c8
Dzientak <Dzientak>
2016-03-16 14:30:02 +0100

    Solver using RK4 implemented. Changed vector sizes and functions arguments. Fixed error caused by incorrect concatenation.

--- main.m
+++ main.m
@@ -6,48 +6,51 @@ format compact; format long e;
 %T = 10;  % time of simulation
 %x0 = [0, 0, 0.00000005,0, 0,0,-0.0000005,0]; % position x, velocity x, position y, velocity y 
 %x0=[0; 0; 3.14151; 0; 0; 0; -3.14159269; 0];
-x0=[0; 1; 1; 0; 0; -1; -1; 0];
+%x0=[0; 1; 1; 0; 0; -1; -1; 0];
+x0=[0 0 0 0 0 0 0 0];
 % models parameters
 M = 0.040;      % mass of ball, kg
 R = 0.0015;     % Radius of ball, m
 I = 2/5*M*R^2;  % Moment of inertia of ball
 g = 9.81;       % gravitational acceleration
 
-% a1=0.00001;
-% a2=0.00001;
+a1=0.00001;
+a2=0.00001;
+freq2=2;
+freq1=freq2;
+phi1=1*pi/2;
+phi2=1*pi/2;
+
+% a1=0;
+% a2=0;
 % freq2=2;
 % freq1=freq2;
 % phi1=1*pi/2;
 % phi2=2*pi/2;
-
-a1=0;
-a2=0;
-freq2=2;
-freq1=freq2;
-phi1=1*pi/2;
-phi2=2*pi/2;
 
 T=10;
 % control function - angular position of table
 step = 50;  % number of przedzia³y strukturalne -> znaleœæ t³umaczenie
 tau = linspace(0, T, step+1);
 u = [a1*sin(freq1*tau+phi1); a2*sin(freq2*tau+phi2)];
-% 
+%
+%u=[linspace(0,T,step+1);linspace(0,T,step+1)];
 % % step in rk4, ~0.01
-% dtau = diff(tau);
-% h0 = 0.01;
-% n = ceil(dtau/h0);
-% cn = cumsum([1;n]);  % cn(1)=1 - numer wez³a rk w którym wypada pierwszy weze³ strukturalny; cn(2)=1+n(1) - numer wez³a rk w którym wypada 2 wze³ strukturalny, ....
+dtau = diff(tau); %wektor roznic - szerokosci przedzialow
+h0 = 0.01; %krok symulacji
+n = ceil(dtau/h0); % liczba krokow symulacji w przedziale
+cn = cumsum([1,n]);  % cn(1)=1 - numer wez³a rk w którym wypada pierwszy weze³ strukturalny; cn(2)=1+n(1) - numer wez³a rk w którym wypada 2 wze³ strukturalny, ....
 
-% x = zeros(cn(end), 2);  % memory for solution
-% t = zeros(cn(end), 1);  % vector of time       
+x = zeros(cn(end), 8);  % memory for solution
+t = zeros(cn(end), 1);  % vector of time       
+u_out = zeros(cn(end), 2);  % vector of control
 %% Solving
 % sim('model.mdl',10);
 
 
-[t x]=ode45(@(t, x) rhs(t, x, u, M, R, I, g),tau,x0);
+%[t x]=ode45(@(t, x) rhs(t, x, u, M, R, I, g),tau,x0);
 
-
+[t,x] = solver(n,dtau, cn, x, t, u, M, R, I, g);
 
 
 
@@ -60,8 +63,8 @@ title('polozenie kulki');
 subplot(2,3,2);
 hold on;
 
-plot(t,u(1,:),'r');
-plot(t,u(2,:),'g');
+plot(tau, u(1,:),'r');
+plot(tau, u(2,:),'g');
 xlabel('t');
 ylabel('u_1, u_2');
 hold off;
--- solver.m
+++ solver.m
@@ -1,4 +1,4 @@
-function [] = solver()
+function [t,x] = solver(n,dtau, cn, x, t, u, M, R, I, g)
 
 for j = 1:length(n)
    % RK4 steps
@@ -8,16 +8,16 @@ for j = 1:length(n)
    h6 = h/6;
    
    for i = cn(j):(cn(j+1)-1)
-    dx1 = rhs(x(i,:),u(j));
+    dx1 = rhs(t(i),x(i,:),u(:,j),M, R, I, g);
     x1 = x(i,:)+h2*dx1;
     
-    dx2 = rhs(x1, u(j));
+    dx2 = rhs(t(i),x1, u(:,j),M, R, I, g);
     x2 = x(i,:)+h2*dx2;
     
-    dx3 = rhs(x2, u(j));
+    dx3 = rhs(t(i), x2,u(:,j),M, R, I, g);
     x3 = x(i,:)+h*dx3;  % krok o pe³nej d³ugosci
-    
-    dx4 = rhs(x3, u(j)); % granica lewostronna!!! sterowania
+   
+    dx4 = rhs(t(i), x3, u(:,j),M, R, I, g); % granica lewostronna!!! sterowania
     
     x(i+1,:) = x(i,:)+h3*(dx2+dx3)+h6*(dx1+dx4);
     t(i+1) = t(i)+h;
