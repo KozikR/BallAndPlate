073ee2010b4a2cab995d5e2faa8f9f5a48b7b25f
Zaneta Blaszczuk <zaneta.blaszczuk@gmail.com>
2016-05-18 14:23:34 +0200

    BFGS revision

--- BFGS.m
+++ BFGS.m
@@ -1,4 +1,6 @@
-function x = BFGS(x0)
+function x = BFGS(x0, )
+
+%STEP 1 - initial conditions
 ep1 = 1e-10;
 ep2 = 1e-10;
 
@@ -12,12 +14,13 @@ x_s = x0;
 gradQ_s = x0;
 
 while 1
+    %STEP 2 - first STOP condition
     gradQ = gradientQ(x);
-
     if gradQ'*gradQ <= ep1 
         break;
     end
 
+    %STEP 3 - setting direction as vector d
     if R == 1
        W = eye(length(x)); 
     else
@@ -26,16 +29,19 @@ while 1
        W = W+(r*r')/(s'*r)-(W*(s*s')*W)/(s'*W*s);
     end
 
-    d = -gradQ\W;
+    d = -gradQ\W; % should be left side division
     
+    %STEP 4 - check if direction is worth searching on 
     if d'*gradQ >= 0
        R = 1;
        continue;
     end
     
+    %STEP 5 - saving old previous values
     x_s = x;
     gradQ_s = gradQ;
     
+    %STEP - searching for x od d direction
     % contraction
     max_contraction = 100;
     lambda = 1;
@@ -50,18 +56,22 @@ while 1
     x = x+lambda*d;
     Qx = Qn;
     
-    if abs(x-x_s) < ep2
-       if R == 0
+    % STEP 7 - checking STOP conditions
+    if abs(x-x_s) < ep2 % if there was no improvement
+       if R == 1 % if it was first iteration after refreshment 
            break;
        else
-           R = 0;
+           R = 1; % start refreshment 
+           continue;
        end
-    end   
+    else % if there was improvment change Refreshment flag to 0 
+       R = 0;
+    end 
+    
     
     % change number of swiches
     % if change R=1 and continue
     % 
+
     
-    
-    R = 0;
 end
