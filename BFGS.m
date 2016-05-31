9b2955c51592bb85fe0c4c21c001f6d34006f17c
Żaneta Błaszczuk <zaneta.blaszczuk@gmail.com>
2016-05-31 22:21:04 +0200

    changes connected with contraction, plotting and debugging

--- BFGS.m
+++ BFGS.m
@@ -15,6 +15,7 @@ tau1_s = tau1;
 tau2_s = tau2;
 
 Qx = q_cost_BB(h0, tau1, tau2, u0, M, Rad, I, g, l, a_max, x0, k, T);
+disp(Qx)
 R = 1;
 
 gradQ_s = [tau1, tau2];
@@ -58,7 +59,7 @@ disp('step2');
     %STEP - searching for x od d direction
     % contraction
     disp('step6');
-    max_contraction = 100;
+    max_contraction = 1000;
     
     d1=[0, d(1:length(tau1)), 0];
     d2=[0, d(length(tau1)+1:end), 0];
@@ -67,22 +68,35 @@ disp('step2');
     dtau1=diff([0 tau1 T]);
     dtau2=diff([0 tau2 T]);
 
+    lambda1 =min([abs(dtau1/dd1),1]);
+    lambda2 =min([abs(dtau2/dd2), 1]);
 
-    lambda = 1e-7;
-    tau1_ = tau1+lambda*d(1:length(tau1));
-    tau2_ = tau2+lambda*d(length(tau1)+1:end);
-    Qn = q_cost_BB(h0, tau1_, tau2_, u0, M, Rad, I, g, l, a_max, x0, k, T);
-    while max_contraction > 0 && Qn > Qx
-       max_contraction = max_contraction-1;
-       lambda = lambda / 2;
-       tau1_ = tau1+lambda*d(1:length(tau1));
-       tau2_ = tau2+lambda*d((length(tau1)+1):(length(tau1)+length(tau2)));
-       Qn = q_cost_BB(h0, tau1_, tau2_, u0, M, Rad, I, g, l, a_max, x0, k, T);
+%     lambda = 1;
+%     tau1_ = tau1+lambda*d(1:length(tau1));
+%     tau2_ = tau2+lambda*d(length(tau1)+1:end);
+%     Qn = q_cost_BB(h0, tau1_, tau2_, u0, M, Rad, I, g, l, a_max, x0, k, T);
+    while max_contraction > 0,% && Qn > Qx
+        max_contraction = max_contraction-1;
+        lambda1 = lambda1 / 2;
+        lambda2 = lambda2 / 2;
+        tau1_ = tau1-lambda1*d(1:length(tau1));
+        tau2_ = tau2-lambda2*d((length(tau1)+1):(length(tau1)+length(tau2)));
+        Qn = q_cost_BB(h0, tau1_, tau2_, u0, M, Rad, I, g, l, a_max, x0, k, T);
+        if(Qn < Qx)
+            tau1_opt = tau1_;
+            tau2_opt = tau2_;
+            break;
+        end
     end
     
-    tau1 = tau1_;
-    tau2 = tau2_;
+    tau1 = tau1_opt;
+    tau2 = tau2_opt;
+    if(Qn>Qx) 
+        disp('STOP3 - no impovement');
+        break;
+    end
     Qx = Qn;
+    disp(Qx);
     
     % STEP 7 - checking STOP conditions
     disp('step7');
@@ -97,6 +111,7 @@ disp('step2');
     else % if there was improvment change Refreshment flag to 0 
        R = 0;
     end 
+    
     
     
     % change number of swiches
--- main_BB.m
+++ main_BB.m
@@ -18,12 +18,14 @@ k=1;
 T=10;
 h0 = 0.01; % simulation step
 
-x0=[0 1 0 0.5 0 0 -0.25 0 0]; %test 1
+x0=[50 0 0 0 50 0 0 0 0]; %test 1
 xf=[0 0 0 0 0 0 0 0 0];
 
 u0 = [u_max, u_max]';
 tau1 = [1, 2, 3, 4, 5, 6, 7];
-tau2 = [2, 6];
+steps=5;
+tau2 = linspace(0, T, steps);
+tau1 = linspace(0, T, steps);
 % tau1 = [0, T];
 % tau2 = [0, T];
 %% Solver bang-bang
@@ -45,22 +47,39 @@ tau2_0 = tau2;
 
 
 %% Plot
-subplot(321)
+plot(x0(1),x0(5), '*r');
+hold on
+plot(xf(1),xf(5),'*g');
 plot(x(:,1),x(:,5),'Linewidth',2);
 xlabel('x');
 ylabel('y');
 title('Po³o¿enie kulki');
+axis square
+axis([-100 100 -100 100]);
+legend('punkt pocz¹tkowy','punkt docelowy');
+
+figure;
+
+subplot(321)
+stairs(cumsum(dtau), u_out(1,:),'r','Linewidth',2);
+xlabel('t');
+ylabel('u_1');
+% axis([0 T -1 1]);
+hold off;
+title('Przyspieszenie k¹towe stolika');
+% legend('u_1','u_2');
 subplot(322);
 hold on;
 
-plot(cumsum(dtau), u_out(1,:),'r','Linewidth',2);
-plot(cumsum(dtau), u_out(2,:),'g','Linewidth',2);
+
+
+stairs(cumsum(dtau), u_out(2,:),'g','Linewidth',2);
 xlabel('t');
-ylabel('u_1, u_2');
-axis([0 T -1 1]);
+ylabel('u_2');
+% axis([0 T -1 1]);
 hold off;
 title('Przyspieszenie k¹towe stolika');
-legend('u_1','u_2');
+% legend('u_1','u_2');
 subplot(323);
 hold on;
 
@@ -69,7 +88,7 @@ plot(t,x(:,5),'g','Linewidth',2);
 xlabel('t');
 ylabel('x_1, x_5');
 hold off;
-axis([0 T -5 5]);
+% axis([0 T -5 5]);
 title('Po³o¿enie kulki w osi x i y');
 subplot(324);
 hold on;
@@ -78,7 +97,7 @@ plot(t,x(:,6),'g','Linewidth',2);
 xlabel('t');
 ylabel('x_2, x_6');
 hold off;
-axis([0 T -3 3]);
+% axis([0 T -3 3]);
 title('Prêdkoœæ kulki w osi x i y');
 subplot(325);
 hold on;
@@ -88,7 +107,7 @@ plot(t,x(:,7),'g','Linewidth',2);
 xlabel('t');
 ylabel('x_3, x_7');
 hold off;
-axis([0 T -0.5 0.5]);
+% axis([0 T -0.5 0.5]);
 title('K¹t nachylenia stolika w osi x i y');
 subplot(326);
 hold on;
@@ -97,7 +116,7 @@ plot(t,x(:,8),'g','Linewidth',2);
 xlabel('t');
 ylabel('x_4, x_8');
 hold off;
-axis([0 T -1 1]);
+% axis([0 T -1 1]);
 title('Prêdkoœæ k¹towa stolika w osi x i y');
 %%
 figure;
