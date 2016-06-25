8027c23f2dc30dc63c12c32a2fb83c79d43647c7
Żaneta Błaszczuk <zaneta.blaszczuk@gmail.com>
2016-06-25 23:43:50 +0200

    warning fixed, different initial contidions

--- main_single_BFGS.m
+++ main_single_BFGS.m
@@ -16,8 +16,8 @@ a_max = 30*pi/180; % max angle of table, radians
 u_max = 0.1;
 k=1;
 
-T=4;
-h0 = 0.01; % simulation step
+T=2.8;
+h0 = 0.001; % simulation step
 
 x0=[0.1 0 0 0 0.1 0 0 0 0]; %test 1
 xf=[0 0 0 0 0 0 0 0 0];
@@ -26,12 +26,9 @@ u0 = [u_max, u_max]';
 
 
 %%  BFSG
- x0=[0.1 0 0 0 0.02 0 0 0 0];
-T=2.5;
-
-steps=6;
+steps=3;
     tau2_0 = linspace((1/(steps+1))*T, (steps/(steps+1))*T, steps);
-    tau1_0 = linspace((1/(steps+1))*T, (steps/(steps+1))*T, steps+2);
+    tau1_0 = linspace((1/(steps+1))*T, (steps/(steps+1))*T, steps);
 
     [tau1, tau2, x, psi, t, Q,u0] = BFGS(tau1_0, tau2_0, h0, u0, B, g, l, a_max, x0, k, xf, T);
     disp(Q);
@@ -118,7 +115,7 @@ figure;
 plot(t,[psi(:,4),psi(:,8)])
 xlabel('t');
 ylabel('\psi');
-legend('\psi_4','\psi_8','\psi_3','\psi_4','\psi_5','\psi_6','\psi_7','\psi_8');
+legend('\psi_4','\psi_8');
 
 u1=zeros(length(tau1)+2,1);
 u2=zeros(length(tau2)+2,1);
